1b7f76d3d4238938982928dd78e4e66bdd5715f7
SeokyungKim <kskyung0624@gmail.com>
2023-11-14 20:36:33 +0900

    tutorial end

---
+++ src/main/java/com/soko/security/auth/AuthenticationController.java
@@ -0,0 +1,32 @@
+package com.soko.security.auth;
+
+import com.soko.security.auth.AuthenticationResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+        @RequestBody RegisterRequest request
+    ) {
+       return ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authentication(
+        @RequestBody AuthenticationRequest request
+    ) {
+        // TODO
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+}
---
+++ src/main/java/com/soko/security/user/Role.java
@@ -0,0 +1,6 @@
+package com.soko.security.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
